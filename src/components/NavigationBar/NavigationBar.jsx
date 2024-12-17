72c6d840ba2c8c91a25d02a3d6d47d2e1c6fb576
Arthu085 <arthurghizi22@gmail.com>
2024-12-17 09:21:24 -0300

    adicionado botão de logout

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import hamburguer from '../../assets/hamburguer.svg';
 import './NavigationBar.css';
-import SideBar from '../SideBar/SideBar'; // Importa a SideBar
+import SideBar from '../SideBar/SideBar';
+import logout from '../../assets/logout-svgrepo-com.svg'
 
 const NavigationBar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -17,15 +18,21 @@ const NavigationBar = () => {
         <Link className="link-dashboard" to="/dashboard">
           Dashboard
         </Link>
-        <button
-          className="hamburguer-button"
-          id="toggle-sidebar"
-          onClick={toggleSidebar}
-        >
-          <img src={hamburguer} alt="Menu" />
-        </button>
+        <div className='buttons'>
+          <button
+            className="hamburguer-button"
+            id="toggle-sidebar"
+            onClick={toggleSidebar}
+          >
+            <img src={hamburguer} alt="Menu" />
+          </button>
+          <Link to='/'>
+            <button className='logout-button' id='logout-button'>
+              <img src={logout} alt="Saída" />
+            </button>
+          </Link>
+        </div>
       </header>
-      {/* Passa o estado e a função para a SideBar */}
       <SideBar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
     </div>
   );
